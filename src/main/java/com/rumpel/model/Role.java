d087f5ec3cdceb4efbff7d973ff77a37b78bfda0
rumpel <artem.sinkovskiy@gmail.net>
2018-04-29 22:53:42 +0300

    add bootstrap /greeting

---
+++ src/main/java/com/rumpel/model/Role.java
@@ -0,0 +1,5 @@
+package com.rumpel.model;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/com/rumpel/repos/UserRepo.java
@@ -0,0 +1,9 @@
+package com.rumpel.repos;
+
+
+import com.rumpel.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
